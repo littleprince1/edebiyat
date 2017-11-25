cf6ae6f52bdc2dcc3c60429e9d98c001950790c4
Yusuf Topkaya <yusuf-topkaya@hotmail.com>
2017-11-25 17:10:45 +0300

    Login Page

---
+++ Edebiyat/Edebiyat/U_Kontroller/CustomTextBox.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Edebiyat.U_Kontroller
+{
+    /// <summary>
+    /// CustomTextBox.xaml etkileşim mantığı
+    /// </summary>
+    public partial class CustomTextBox : UserControl
+    {
+        public CustomTextBox()
+        {
+            InitializeComponent();
+            tb.Text = Tip;
+            tb.Foreground = Brushes.Gray;
+        }
+        private string tip;
+
+        public string Tip
+        {
+            get { return tip; }
+            set { tip = value; tb.Text = tip; }
+        }
+
+
+        private void tb_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tb.Text == Tip)
+            {
+                tb.Clear();
+                var bc = new BrushConverter();
+                tb.Foreground = (Brush)bc.ConvertFrom("#e0f8f4"); ;
+            }
+        }
+
+        private void tb_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (tb.Text.Length == 0)
+            {
+                tb.Text = Tip;
+                tb.Foreground = Brushes.Gray;
+            }
+        }
+    }
+}
